7dc3cb84763328305bbaef0edb860451f8311c3f
JuanMa Garrido <JuanMa.Garrido@gmail.com>
2017-05-18 15:50:01 +0200

    edit data

--- app.js
+++ app.js
@@ -9,7 +9,9 @@ const app = express()
 const PORT = 3000
 
 app.use(express.static('public'))
+
 app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
 app.set('view engine', 'pug')
 
@@ -28,8 +30,15 @@ app.use((req, res, next) => {
 
 app.get('/cart', (req, res) => {
   const cart = req.session.cart
-  if (cart.length) {
-    res.render('cart/index', { cart })
+  const detailedCart = cart.map( cartItem => {
+    let aProductsIdFiltered = products.filter( item => cartItem.id === item.id )
+    let detailsProduct = aProductsIdFiltered[0]
+    detailsProduct.quantity = cartItem.quantity
+    return detailsProduct
+  } )
+
+  if (detailedCart.length) {
+    res.render('cart/index', { cart: detailedCart })
   } else {
     res.render('cart/empty')
   }
@@ -40,15 +49,20 @@ app.get('/', (req, res) => {
   res.render('list/index', { products, cart })
 })
 
-app.post('/cart', (req, res) => {
-  const { id, name, price, image } = req.body
-  const newItem = { id, name, price, image }
-  req.session.cart.push(newItem)
+app.post('/cart-native', (req, res) => {
+  const { id, quantity } = req.body
+  req.session.cart.push({ id, quantity })
   res.redirect('/')
 })
 
+app.post('/cart-ajax', (req, res) => {
+  const { id, quantity } = req.body
+  req.session.cart.push({ id, quantity })
+  res.status(200).send(`${quantity} items w/ ID: ${id} were added to the cart`)
+})
+
 app.delete('/cart/:id', (req, res) => {
-  const id = +req.params.id
+  const id = req.params.id
   const originalSize = req.session.cart.length
   req.session.cart = req.session.cart.filter(item => {
     return item.id !== id
@@ -58,6 +72,18 @@ app.delete('/cart/:id', (req, res) => {
   } else {
     res.status(200).send(`Item w/ ID: ${id} was removed succesfully`)
   }
+})
+
+app.put('/cart/:id', (req, res) => {
+  const id = req.params.id
+  const quantity = req.body.quantity
+  req.session.cart = req.session.cart.map(item => {
+    if (item.id === id) {
+      item.quantity = quantity
+    }
+    return item
+  })
+  res.status(200).send(`Items w/ ID: ${id} was succesfully updated`)
 })
 
 app.listen(PORT, () => console.log(`Listening on PORT ${PORT}`))
--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,5 @@
 console.log('this file is loaded...')
+
 $('a.remove').on('click', function (e) {
   e.preventDefault()
   const url = $(this).attr('href')
@@ -15,3 +16,42 @@ $('a.remove').on('click', function (e) {
     console.log(err)
   })
 })
+
+$('.add-to-cart-ajax').on('submit', function (e) {
+  e.preventDefault()
+  const url = '/cart-ajax'
+  const id = $(this).find("[name='id']").val()
+  const quantity = $(this).find("[name='quantity']").val()
+  $.ajax({
+    url,
+    data: { id, quantity },
+    method: 'POST'
+  })
+  .done(function (msg) {
+    alert(msg)
+    window.location.reload()
+  })
+  .fail(function (err) {
+    console.log(err)
+  })
+})
+
+$(".list-cart .quantity").on("keyup mouseup", function() {
+  const quantity = $(this).val()
+  const id = $(this).siblings('[name="id"]').val()
+  const url = `/cart/${id}`
+
+  $.ajax({
+    url,
+    data: { quantity },
+    method: 'PUT'
+  })
+  .done(function (msg) {
+    console.log(msg)
+  })
+  .fail(function (err) {
+    console.log(err)
+  })
+})
+
+
